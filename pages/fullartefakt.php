46c3724b1dc25ce73f391aeb67df35f6fdbc5877
krouse212 <kraine212@gmail.com>
2017-10-31 15:03:48 +0300

    Initial commit

---
+++ pages/fullartefakt.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="utf-8">
+
+	<title>Артефакт</title>
+
+
+					<!-- Bootstrap -->
+		
+		<link href="../adaptive/css/bootstrap.min.css" rel="stylesheet"/>
+		
+							<!-- Fonts -->
+
+		<link href="../fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+				<!-- CSS -->
+		<link href="../css/main.css" rel="stylesheet">
+		<link href="../css/news.css" rel="stylesheet">	
+		<link href="../css/catalog.css" rel="stylesheet">
+		<link href="../css/fullartefakt.css" rel="stylesheet">		
+
+
+</head>
+<body>
+
+<?php
+
+include '/includs/header.php';
+
+?>
+
+
+<div class="content">
+
+<p class="title_1">Главная  »  Услуги  »  Жирная кожа</p>
+<p class="bold title_2">Жирная кожа</p>
+
+</div>
+
+
+<div class="news_body">
+
+<div class="first_ban"> 
+<div class="content">
+
+
+
+
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+</div>
+
+<div class="banner_fullartefakt">
+	
+</div>
+
+
+
+<div class="catalog_fullartefakt">
+<div class="bold b2_caption first_text malnull adres_city">
+<p class="partners_li_icon"></p>Каталог
+</div>
+
+
+	
+<div class="b2_text mar_contact_adres">
+<div class="partners_li">
+
+<div class="partners_li_text adres_place"> Как сообщает китайский ресурс MyDrivers, руководитель Foxconn рассказал, что в новом iPhone Apple решила использовать безрамочный OLED-дисплей</div>
+	
+</div>
+
+
+<div class="bold catalog_category">
+
+<a href="#">
+<p class="catalog_category_activ catalog_categorys">Лицо</p>
+</a>
+
+<a href="#">
+<p class="catalog_category_hover catalog_categorys">Тело</p>
+</a>
+	
+</div>
+</div>
+</div>
+
+<div class="clear">
+</div>
+
+<br>
+<br>
+<div class="content">
+<div class="row">
+<div class="col-lg-6 col-lg-offset-0 col-md-12">
+
+
+<div id='one' class="catalog_product">
+	
+<div class="catalog_product_img">
+	<img src="../images/catalog/1.jpg" alt="" class="catalog_img">
+</div>
+
+
+
+
+
+
+<p  class="bold category_name">PBSerum Daily Anti-Wrinkle</p>
+
+<p class="bold category_description">tratamiento:Tratamiento anti arrugas</p>
+
+
+<div class="price">
+	
+	<div class="fedb_button_catalog bold">
+
+		подробнее
+	</div>
+
+<div id='two'  class="bold catalog_price" >
+	45,00 €
+</div> 
+
+</div>
+
+
+</div>
+
+
+</div>
+
+<div class="col-lg-6 col-lg-offset-0 col-md-12">
+
+
+<div id='three' class="catalog_product">
+	
+<div class="catalog_product_img">
+	<img src="../images/catalog/1.jpg" alt="" class="catalog_img">
+</div>
+
+
+
+
+
+
+<p  class="bold category_name">PBSerum Daily Anti-Wrinkle</p>
+
+<p class="bold category_description">tratamiento:Tratamiento anti arrugas</p>
+
+
+<div class="price">
+	
+	<div class="fedb_button_catalog bold">
+
+		подробнее
+	</div>
+
+<div id='four'  class="bold catalog_price" >
+	45,00 €
+</div> 
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+<div class="col-lg-6 col-lg-offset-0 col-md-12">
+
+
+<div id='five' class="catalog_product">
+	
+<div class="catalog_product_img">
+	<img src="../images/catalog/1.jpg" alt="" class="catalog_img">
+</div>
+
+
+
+
+
+
+<p  class="bold category_name">PBSerum Daily Anti-Wrinkle</p>
+
+<p class="bold category_description">tratamiento:Tratamiento anti arrugas</p>
+
+
+<div class="price">
+	
+	<div class="fedb_button_catalog bold">
+
+		подробнее
+	</div>
+
+<div id='six'  class="bold catalog_price" >
+	45,00 €
+</div> 
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+
+
+<div class="col-lg-6 col-lg-offset-0 col-md-12">
+
+
+<div id='seven' class="catalog_product">
+	
+<div class="catalog_product_img">
+	<img src="../images/catalog/1.jpg" alt="" class="catalog_img">
+</div>
+
+
+
+
+
+
+<p  class="bold category_name">PBSerum Daily Anti-Wrinkle</p>
+
+<p class="bold category_description">tratamiento:Tratamiento anti arrugas</p>
+
+
+<div class="price">
+	
+	<div class="fedb_button_catalog bold">
+
+		подробнее
+	</div>
+
+<div id='eight'  class="bold catalog_price" >
+	45,00 €
+</div> 
+
+</div>
+
+
+</div>
+
+
+</div>
+
+</div>
+</div>
+
+
+
+<!-- footer -->
+
+<?php
+
+include '/includs/footer.php';
+
+?>
+
+			<!-- jQuery -->
+
+	<script src="../js/jquery.min.js"></script>
+
+			<!-- bootstrap -->	
+
+	<script src="../adaptive/js/bootstrap.min.js"></script>
+
+  
+			<!-- JavaScript -->
+
+
+	<script src="../js/main.js"></script>
+
+
+</body>
+</html>
